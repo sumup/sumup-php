caebecca695960f42ed575f43f2510a3637f3bce
appscisumup <30830081+appscisumup@users.noreply.github.com>
2026-09-12 01:32:25 +0200

    chore: synced file(s) with sumup/apis (#114)

--- src/Checkouts/Checkouts.php
+++ src/Checkouts/Checkouts.php
@@ -391,6 +391,43 @@ class Checkouts implements SumUpService
     }
 
     /**
+     * Process a checkout
+     *
+     * @param string $checkoutId Unique identifier of the checkout resource.
+     * @param \SumUp\Types\ProcessCheckout|array<string, mixed> $body Required request payload
+     * @param RequestOptions|null $requestOptions Optional typed request options
+     *
+     * @return mixed|\SumUp\Types\CheckoutAccepted
+     * @throws \SumUp\Exception\ApiException
+     * @throws \SumUp\Exception\UnexpectedApiException
+     * @throws \SumUp\Exception\ConnectionException
+     * @throws \SumUp\Exception\SDKException
+     */
+    public function process(string $checkoutId, \SumUp\Types\ProcessCheckout|array $body, ?RequestOptions $requestOptions = null)
+    {
+        $path = sprintf('/v0.1/checkouts/%s', rawurlencode((string) $checkoutId));
+        $payload = [];
+        $requestBody = $body;
+        if (is_array($requestBody)) {
+            $requestBody = \SumUp\Types\ProcessCheckout::fromArray($requestBody);
+        }
+        $payload = RequestEncoder::encode($requestBody);
+        $headers = RequestHeaders::build($this->accessToken, $requestOptions);
+
+        $response = $this->client->send('PUT', $path, $payload, $headers, $requestOptions);
+
+        return ResponseDecoder::decodeOrThrow($response, [
+            '200' => ['type' => 'mixed'],
+            '202' => ['type' => 'class', 'class' => \SumUp\Types\CheckoutAccepted::class],
+        ], [
+            '400' => ['type' => 'mixed'],
+            '401' => ['type' => 'class', 'class' => \SumUp\Types\Problem::class],
+            '404' => ['type' => 'class', 'class' => \SumUp\Types\Error::class],
+            '409' => ['type' => 'class', 'class' => \SumUp\Types\Error::class],
+        ], 'PUT', $path);
+    }
+
+    /**
      * Update a checkout
      *
      * @param string $checkoutId Unique identifier of the checkout resource.
